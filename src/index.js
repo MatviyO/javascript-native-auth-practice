4effa3daed43eadd632c27e56808a7eaab925730
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-06-06 23:02:27 +0300

    fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import './styles.css'
+import {isValid} from "./utils";
+import {Question} from "./question";
+
+const form = document.getElementById('form')
+const input = form.querySelector('#question-input')
+const button = form.querySelector('#submit')
+
+form.addEventListener('submit', submitFOrmHandler)
+input.addEventListener('input', () => {
+    button.disabled = !isValid(input.value)
+})
+
+function submitFOrmHandler(event) {
+    event.preventDefault()
+
+    if( isValid(input.value)) {
+        const question = {
+            text: input.value.trim(),
+            date: new Date().toJSON()
+        }
+        button.disabled = true
+        Question.create(question).then(() => {
+            input.value = ''
+            input.className = ''
+            button.disabled = false
+        })
+    }
+}
